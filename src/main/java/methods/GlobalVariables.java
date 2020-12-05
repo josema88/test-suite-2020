a7ef42f7da1bb4e6b56ab36442dd80705ad67f18
Jose Ordoñez <josemaord@gmail.com>
2020-12-04 23:24:29 -0600

    add sample global variables

--- src/main/java/methods/GlobalVariables.java
+++ src/main/java/methods/GlobalVariables.java
@@ -1,7 +1,10 @@
 package methods;
 
 public class GlobalVariables {
-    public static String urlRoot = "<YOUR_WEBAPP_URL>";
+    //Your Web App URL
+    public static String urlRoot = "https://web-app-qa-meetup.azurewebsites.net/";
+    //Your email
     public static String reportReceiverEmail = "<EMAIL>";
+    //Selenium Grid Hub URL
     public static String seleniumGridHubUrl = "http://selenium-hub:4444/wd/hub";
 }
